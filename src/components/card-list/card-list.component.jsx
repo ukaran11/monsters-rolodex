cf827b968ebe263b24771516b57ed8dd37d518ba
Utsav Karan <ukaran11@gmail.com>
2022-01-23 00:22:00 +0530

    Utsav | Added card comp and styling

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Card } from '../card/card.component';
 import './card-list.styles.css';
 
 export const CardList = (props) => {
     console.log(props)
-    return (<div className='card-list'>{props.children}</div>)
+    return (
+     <div className='card-list'>
+         { props.monsters.map(monster => (
+          <Card monster={monster} />
+        ))}
+     </div>
+    )
 }
